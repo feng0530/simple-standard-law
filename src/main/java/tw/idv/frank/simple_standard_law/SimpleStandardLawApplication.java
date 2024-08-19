f8feec58e91362fb2b5d2788e17edd7eed46dd2a
23001211JAMESWE\Frank <fff80705@gmail.com>
2024-08-19 14:39:37 +0800

    Create project

---
+++ src/main/java/tw/idv/frank/simple_standard_law/SimpleStandardLawApplication.java
@@ -0,0 +1,13 @@
+package tw.idv.frank.simple_standard_law;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStandardLawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleStandardLawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/idv/frank/simple_standard_law/SimpleStandardLawApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.idv.frank.simple_standard_law;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStandardLawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
